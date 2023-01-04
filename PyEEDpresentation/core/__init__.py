1522d6675753dd422f32a36ae48ac1bd813cf8c4
maxim945 <75306928+maxim945@users.noreply.github.com>
2023-01-04 10:39:38 +0100

    Delete PyEEDpresentation directory

--- PyEEDpresentation/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dnasequence import DNASequence
-from .proteinsequence import ProteinSequence
-
-__doc__ = "PyEED is a Python-encoded data model of an Enzyme Engineering Database. It supports the scalable and reproducible analysis of sequence and structure data of protein families, and makes data and processes findable, accessible, interoperable, and reusable according to the FAIR data principles."
-
-__all__ = [
-    "DNASequence",
-    "ProteinSequence",
-]
